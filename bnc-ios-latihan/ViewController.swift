388f7fb86d7e6ee2f2f736f34f00641221f1a78c
Muhamad Taufiq Ramadhan <muhamad.ramadhan@ottodigital.id>
2022-02-18 13:39:28 +0700

    Initial Commit

---
+++ bnc-ios-latihan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bnc-ios-latihan
+//
+//  Created by Muhamad Taufiq Ramadhan on 18/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
